cf5b29606afc2a4f6a0ec4fec22a18ab1fcbc0ad
Adeel McDangerson <adeelsk4@gmail.com>
2021-02-06 16:37:55 +0000

    React music player, initial styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//importing styling
+import "./styles/App.scss";
+//adding components
+import Player from "./components/Player";
+import Song from "./components/Song";
 
+//main app function
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Song />
+      <Player />
+      ''
     </div>
   );
 }
